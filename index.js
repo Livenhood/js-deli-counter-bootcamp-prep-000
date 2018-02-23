2236eb8d297dcc8472bdbf1dfba53b5deeed263b
Michelle Livengood <ml2876@nyu.edu>
2018-02-23 17:20:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,13 +17,14 @@ function nowServing(katzDeli) {
   return (`Currently serving ${katzDeli.shift()}.`);
 }
 
+var line = [];
+
 function currentLine(katzDeli) {
-  let i = 0;
-  while (i<katzDeli.length) {
-    line.push(1)
+  for (let i = 0; i < katzDeli.length; i++) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
   }
-  if (katzDeli.length===0){
+  if (katzDeli.length === 0) {
     return "The line is currently empty.";
   } else
-  return (`The line is currently:`)
+  return(`The line is currently:` + line);
 }
